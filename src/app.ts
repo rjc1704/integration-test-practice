ec286a49360730c9e069cdcfee4f4ecc6aa75e47
Jecheon Ryu <rjc1704@gmail.com>
2025-07-28 18:49:10 +0900

    수정

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello Express!</h1>");
+  res.send("<h1>Hi!!!! Express!</h1>");
 });
 
 app.listen(3000, () => {
