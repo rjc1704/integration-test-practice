1b174223a663522fddd2f5a1a043523c4342ef71
Jecheon Ryu <rjc1704@gmail.com>
2025-07-28 19:44:20 +0900

    수정5

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import "dotenv/config";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send(`<h1>Hiiii Express! DB_HOST: ${process.env.DB_HOST} </h1>`);
+  res.send(`<h1>tar scp! DB_HOST: ${process.env.DB_HOST} </h1>`);
 });
 
 app.listen(3000, () => {
